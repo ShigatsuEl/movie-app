5f55a12863df76c373818507da938361036d5679
MinChan Lee <shigatsu970704@gmail.com>
2020-11-19 11:57:02 +0900

    11.Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -2,32 +2,17 @@ import React from "react";
 
 class App extends React.Component {
   state = {
-    count: 0,
-  };
-  plus = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
+    isLoading: true,
+    movie: [],
   };
   componentDidMount() {
-    console.log("Component Did Mount");
-  }
-  componentDidUpdate() {
-    console.log("Component Did Update");
-  }
-  componentWillUnmount() {
-    console.log("Goobye, Component");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
   render() {
-    console.log("Rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.plus}>Plus</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "Complete"}</div>;
   }
 }
 
