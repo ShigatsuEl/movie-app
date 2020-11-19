f521e5386a0febf2aa6662b675b8e01ae8382014
MinChan Lee <shigatsu970704@gmail.com>
2020-11-19 13:10:21 +0900

    13.Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
     isLoading: true,
-    movie: [],
+    movies: [],
   };
 
   getMovies = async () => {
-    const movie = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    /* 
+    const {data: {data: {movies}}} = await fetch("https://yts-proxy.now.sh/list_movies.json")
+      .then((response) => response.json())
+      .then((data) => console.log(data)); 
+    여기서 사용한 fetch는 위의 axiox와 같은 역할을 한다. 표현방법만 다를 뿐, 어떤 것을 사용해도 문제 되지 않는다.
+    */
+    this.setState({ movies, isLoading: false });
+    console.log(movies);
   };
 
   componentDidMount() {
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading" : "Complete"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading"
+          : movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+      </div>
+    );
   }
 }
 
