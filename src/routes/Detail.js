8950f4682d9b46ec12d60766a45aeb8180da63cc
MinChan Lee <shigatsu970704@gmail.com>
2020-11-21 16:23:31 +0900

    22.Redirecting

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,9 +1,27 @@
-import { func } from "prop-types";
 import React from "react";
+import "./Detail.css";
 
-function Detail(props) {
-  console.log(props);
-  return <span>hello!</span>;
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return (
+        <img
+          src={location.state.poster}
+          alt={location.state.title}
+          className="movie__detail-poster"
+        ></img>
+      );
+    } else {
+      return null;
+    }
+  }
 }
 
 export default Detail;
